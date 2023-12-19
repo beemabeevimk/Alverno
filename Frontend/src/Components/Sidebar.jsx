d7f8eaa452ceed45ae280623bd8c338ba90067c7
Beema Beevi M.K <mariyambeevi1087@gmail.com>
2023-12-19 21:48:46 +0530

    installed and applied tailwind css

--- Frontend/src/Components/Sidebar.jsx
+++ Frontend/src/Components/Sidebar.jsx
@@ -4,7 +4,8 @@ import { FaTh, FaUserAlt, FaCommentAlt, FaBars } from "react-icons/fa";
 import { NavLink } from 'react-router-dom';
 
 export default function Sidebar({children}) {
-  const [isOpen, setIsOpen] = useState()
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
   const menuItem = [
     {
       path:"/",
@@ -29,18 +30,18 @@ export default function Sidebar({children}) {
   ]
   return (
     <div className='container2'>
-       <div className="sidebar">
+       <div style={{width: isOpen ?  "250px" : "50px"}} className="sidebar">
           <div className="top_section">
-          <h1 className="logo">Logo</h1>
-          <div className="bars">
-             <FaBars/>
+          <h1 style={{display: isOpen ?  "block" : "none"}} className="logo">Logo</h1>
+          <div style={{marginLeft: isOpen ?  "50px" : "0px"}} className="bars">
+             <FaBars onClick={toggle} />
           </div>
           </div> 
           {
             menuItem.map((item, index)=>(
               <NavLink to={item.path} key={index} className="link" activeclassName="active">
                 <div className="icon">{item.icon}</div>
-                <div className="link_text">{item.name}</div>
+                <div style={{display: isOpen ?  "block" : "none"}} className="link_text">{item.name}</div>
               </NavLink>
             ))
           }     
--- Frontend/src/pages/StudentSide/Profile.jsx
+++ Frontend/src/pages/StudentSide/Profile.jsx
@@ -3,7 +3,62 @@ import React from 'react'
 const Profile = () => {
   return (
     <div>
-      <h1>Profile page</h1>
+      <div className='container'>
+        <div className='pagination-box bg-gray-300 p-4 flex items-center h-9'>  
+          <h1 className='font-semibold '>Student</h1>
+            <div className="mx-2">&gt;</div>
+          <h1 className='font-semibold '>Profile</h1>
+        </div>
+      
+        <div className='name-container pt-4 '>
+          <div className='name-box bg-gray-300 h-20 flex items-center px-2  '>
+            <div className='profile-icon h-14 w-14 bg-lightBlue-400 rounded-full '></div>
+              <div className='justify-between px-2'>
+                  <h1 className='font-extrabold px-1'>Beema Beevi M.K</h1>
+                    <div className='px-1 pt-1 flex items-center '>
+                      <button className='w-20 h-6 bg-slate-500 font-bold text-white  '>Edit</button>
+                    </div>
+              </div>
+          </div>
+        </div>
+
+        {/* <div className=''>
+        <div className='details-container flex flex-col p-8 '>
+          <div className='bg-gray-300 w-1/3 h-96 flex flex-col justify-center items-center '>
+            <div className='profile-pic bg-orange-500 w-28 h-36 '></div>
+              <div className='flex flex-col items-center pt-2 '>
+                <h1 className='email font-semibold '>mariyam@gmail.com</h1>
+              <h1 className='reg font-semibold'>Reg No. 191445</h1>
+            </div>
+          </div>
+
+        <div className=' flex justify-end items-center pt-4'>
+          <div className='details bg-gray-300 w-1/3 h-96  '></div>  
+        </div> 
+        </div>
+        </div> */}
+
+
+<div className='pt-11 flex flex-row  w-full gap-3'>
+                
+                <div className='mx-8 bg-gray-300 w-1/3 h-96 flex flex-col justify-center items-center '>
+                    <div className='profile-pic bg-orange-500 w-28 h-36 '></div>
+                    <div className='flex flex-col items-center pt-2 '>
+                        <h1 className='email font-semibold '>mariyam@gmail.com</h1>
+                        <h1 className='reg font-semibold'>Reg No. 191445</h1>
+                    </div>
+                </div>
+
+                <div className='mx-8 bg-gray-300 w-2/3 h-96 flex flex-col justify-center items-center '> 
+                    <div className='flex flex-col items-center pt-2 '>
+                        <h1 className='email font-semibold '>mariyam@gmail.com</h1>
+                        <h1 className='reg font-semibold'>Reg No. 191445</h1>
+                    </div>
+                </div>
+              
+   </div>
+
+        </div>
     </div>
   )
 }
